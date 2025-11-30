51cd0b4e51aab2b5cc93dc839c016d4721081318
AnhGam <guardalex8@gmail.com>
2025-11-30 18:14:39 +0700

    Fix giao diện

--- Chinese Chess/Views/GameView.xaml.cs
+++ Chinese Chess/Views/GameView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Chinese_Chess.Views
 {
@@ -20,19 +21,62 @@ namespace Chinese_Chess.Views
     /// </summary>
     public partial class GameView : Window
     {
+        DispatcherTimer gameTimer;
+        int timeInSeconds = 0;
+        bool isPaused = false;
+        bool isMuted = false;
         public GameView()
         {
             InitializeComponent();
         }
 
-        private void Settings_Click(object sender, RoutedEventArgs e)
+        private void Pause_Click(object sender, RoutedEventArgs e)
         {
+            isPaused = !isPaused;
+        }
+
+        private void New_Game_Click(object sender, RoutedEventArgs e)
+        {
+            timeInSeconds = 0;
+            isPaused = false;
+            if (GameTimerLabel != null) GameTimerLabel.Content = "00:00";
 
         }
 
         private void MuteButton_Click(object sender, RoutedEventArgs e)
         {
+            isMuted = !isMuted;
+            string iconName = isMuted ? "sound_off.png" : "sound_on.png";
+            try
+            {
+                var uri = new Uri($"pack://application:,,,/Chinese Chess;component/Assets/Button/{iconName}");
+                SoundIcon.Source = new BitmapImage(uri);
+            }
+            catch { }
+        }
 
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Tính năng Cài đặt đang phát triển!");
+        }
+
+
+        private void Backward_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Chức năng đi lùi (Undo) chưa làm.");
+        }
+
+
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Chức năng đi tiếp (Redo) chưa làm.");
+        }
+
+
+        private void BoardCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
         }
     }
 }
