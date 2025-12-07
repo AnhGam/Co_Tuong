27a4bde8dc7a732ec769fb1af5bcff6bb1f1c812
AnhGam <guardalex8@gmail.com>
2025-12-07 20:44:19 +0700

    HighlightSpot

---
+++ Chinese Chess/Models/HighlightSpot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinese_Chess.Models
+{
+    public class HighlightSpot
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
